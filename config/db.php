a6c239f47af52bb0631dea075b13f5b5d4e8d1cd
Damian Jot <damian.juszczyk17@gmail.com>
2025-09-17 17:14:14 +0200

    uptade style taskora

--- config/db.php
+++ config/db.php
@@ -1,10 +1,15 @@
 <?php
-$host = "";
-$username = "";
-$password = "";
-$dbname = ""; 
+require_once __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
